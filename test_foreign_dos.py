6f3729674df9fd3684238172313d00fe37eae9af
Kadiman Gamage <kadimangamage127@gmail.com>
2026-05-26 16:08:11 +0530

    Update test_foreign_dos.py

--- test_foreign_dos.py
+++ test_foreign_dos.py
@@ -2,12 +2,12 @@ import time
 import random
 from scapy.all import IP, TCP, send
 
-# Target විස්තර (අපේ NIDS එක දිවෙන තැන)
+
 TARGET_IP = "127.0.0.1"
 TARGET_PORT = 5000
-PACKET_COUNT = 180  # Threshold එක 150 නිසා 180ක් යැවීමෙන් ඇලර්ට් එක වැටේ
+PACKET_COUNT = 180 
 
-# ටෙස්ට් කිරීමට විවිධ රටවල් වල ප්‍රසිද්ධ IP ලිපින කිහිපයක්
+
 FOREIGN_IPS = [
     "8.8.8.8",          # United States (Google DNS)
     "1.1.1.1",          # Australia (Cloudflare)
@@ -17,18 +17,17 @@ FOREIGN_IPS = [
 ]
 
 def simulate_foreign_attack():
-    # ලැයිස්තුවෙන් අහඹු ලෙස එක් විදේශීය IP එකක් තෝරා ගැනීම
+  
     spoofed_source_ip = random.choice(FOREIGN_IPS)
     
     print(f"[*] Simulating Attack from a foreign country...")
     print(f"[*] Spoofed Source IP: {spoofed_source_ip} -> Target: {TARGET_IP}:{TARGET_PORT}")
     print(f"[*] Sending {PACKET_COUNT} raw packets raw via Scapy... Please wait.")
     
-    # Scapy මඟින් IP එක වෙනස් කර (Spoof) එකවර පැකට් විශාල ප්‍රමාණයක් යැවීම
     for _ in range(PACKET_COUNT):
-        # Raw Network Packet එකක් නිර්මාණය කිරීම
+     
         packet = IP(src=spoofed_source_ip, dst=TARGET_IP) / TCP(sport=random.randint(1024, 65535), dport=TARGET_PORT, flags="S")
-        # පැකට් එක නෙට්වර්ක් එකට මුදා හැරීම (verbose=False මඟින් console එක පිරිසිදුව තබා ගනී)
+       
         send(packet, verbose=False)
         time.sleep(0.001)
 
